ec8cda81f69ccbf1f666591fc762479605bf48c8
Arjun Shenoy <arjunshenoy23@gmail.com>
2024-08-08 08:55:54 +0530

    About and Layout

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -15,34 +15,20 @@ export default function AuthorLayout({ children, content }: Props) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-x-2 pb-8 pt-6 md:space-y-15">
-          <p className="text-2xl leading-6 text-gray-700 dark:text-gray-300">
-          
+          <p className=" pl-6 pt-2 text-3xl leading-6 text-gray-700 dark:text-gray-300">
+          * Hi I'm Arjun a.k.a NeuralManacle.<br/><br/>
+          * A software engineer with a generalist approach towards solving problems.<br/><br/>
+          * Proficient in Python, JavaScript and C++. <br/><br/>
+          * I like building scalable web applications. <br/><br/>
+          * I'm actively looking for web development roles.<br/><br/>
+          * I make electronic music apart from code.<br/><br/>
+          * Download My Resume &darr;
           </p>
         </div>
       </div>
-      <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          <div className="flex flex-col items-center space-x-2 pt-8">
-            {avatar && (
-              <Image
-                src={avatar}
-                alt="avatar"
-                width={192}
-                height={192}
-                className="h-48 w-48 rounded-full"
-              />
-            )}
-            <h3 className="pb-2 pt-4 text-2xl text-yellow-400 font-bold leading-8 tracking-tight">{name}</h3>
-            <h4 className="text-purple-900 dark:text-purple-400 text-lg font-bold">{occupation}</h4>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="flex space-x-5 pt-5">
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
-              <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
-            </div>
-          </div>
+      
           
-        </div>
+        
       
     </>
   )
